df5f5b856ee0934394ac4b7348b9bbc7947efad7
joao_kalin <joao.paulo@grupoviadigital.com>
2022-09-26 19:15:41 -0300

    feature: adding product and order class

---
+++ ECommerce/Domain/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Domain.Enums;
+
+namespace ECommerce.Domain.Models
+{
+    public class Order : Entity
+    {
+        public List<Product> Products { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public double TotalPrice { get; set; }
+        public OrderStatus Status { get; set; }    
+
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+
+        public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
